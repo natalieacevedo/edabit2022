dbe767c010c3eca2c6070307c31a3ccfeb10fda5
Natalie Acevedo <natalieacevedo25@gmail.com>
2022-08-06 19:08:19 +0100

    more katas done

--- numberToReversedArray.js
+++ numberToReversedArray.js
@@ -1,7 +1,22 @@
-function numberToReversedArray(arr) {
-    for (let i = arr.length; i < 0; i--``){
-
-    }
+function numberToReversedArray(number) {
+  const toArr = Array.from(String(number));
+  let finalArr = [];
+  for (let i = toArr.length - 1; i >= 0; i--) {
+    finalArr.push(parseInt(toArr[i]));
+  }
+  return finalArr;
 }
 
-console.log(numberToReversedArray([9, 7, 9, 5, 8, 4, 1]));
+console.log(numberToReversedArray(1485979));
+
+const SecondSolution = (number) => {
+  const toArr = Array.from(String(number));
+  let finalArr = [];
+
+  toArr.forEach((el, ind, arr) => {
+    finalArr.push(parseInt(arr[arr.length - 1 - ind]));
+  });
+  return finalArr;
+};
+
+console.log(SecondSolution(623478));
